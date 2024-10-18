73d6011a44ee49305c9b3bf3d7a561ba33c81588
Shamima-Nasrin10 <shamima48nasrin@gmail.com>
2024-10-19 02:26:44 +0600

    Seat Model
    SeatSelected BackEnd

--- Ticket-Booking/app/src/main/java/com/shamnas/ticket_booking/activity/SeatListActivity.java
+++ Ticket-Booking/app/src/main/java/com/shamnas/ticket_booking/activity/SeatListActivity.java
@@ -7,20 +7,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import com.shamnas.ticket_booking.R;
+import com.shamnas.ticket_booking.databinding.ActivitySeatListBinding;
+import com.shamnas.ticket_booking.model.Flight;
 
-public class SeatListActivity extends AppCompatActivity {
+public class SeatListActivity extends BaseActivity {
+    private ActivitySeatListBinding binding;
+    private Flight flight;
+    private double price = 0.0;
+    private int num = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_seat_list);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivitySeatListBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getIntentExtra();
+        initSeatList();
+    }
+
+    private void initSeatList() {
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 7);
+        gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                return (position % 7 == 3) ? 1 : 1;
+            }
         });
+        binding.seatRecyclerView.setLayoutManager(gridLayoutManager);
+    }
+
+    private void getIntentExtra() {
+        flight = (Flight) getIntent().getSerializableExtra("flight");
     }
 }
