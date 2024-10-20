dfd154c9a191d01ff42ffcc59f7fa29a5e7eb516
Shamima-Nasrin10 <shamima48nasrin@gmail.com>
2024-10-20 07:49:09 +0600

    Ticket Detail
    BackEnd Completed, Alhamdulillah

--- Ticket-Booking/app/src/main/java/com/shamnas/ticket_booking/activity/SeatListActivity.java
+++ Ticket-Booking/app/src/main/java/com/shamnas/ticket_booking/activity/SeatListActivity.java
@@ -1,7 +1,9 @@
 package com.shamnas.ticket_booking.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -41,6 +43,17 @@ public class SeatListActivity extends BaseActivity {
 
     private void setVariable() {
         binding.backBtn.setOnClickListener(v -> finish());
+        binding.confirmBtn.setOnClickListener(v -> {
+            if (num > 0) {
+                flight.setPassenger(binding.nameSeatSelectedTxt.getText().toString());
+                flight.setPrice(price);
+                Intent intent=new Intent(SeatListActivity.this, TicketDetailActivity.class);
+                intent.putExtra("flight", flight);
+                startActivity(intent);
+            } else {
+                Toast.makeText(SeatListActivity.this, "Please select your seat.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void initSeatList() {
@@ -69,23 +82,22 @@ public class SeatListActivity extends BaseActivity {
                 row++;
             }
             if (i % 7 == 3) {
-              seatList.add(new Seat(Seat.SeatStatus.EMPTY, String.valueOf(row)));
-            }
-            else {
-                String seatName=seatAlphabetMap.get(i%7)+row;
-                Seat.SeatStatus seatStatus=flight.getReservedSeats().contains(seatName)? Seat.SeatStatus.UNAVAILABLE: Seat.SeatStatus.AVAILABLE;
-                seatList.add(new Seat(seatStatus,seatName));
+                seatList.add(new Seat(Seat.SeatStatus.EMPTY, String.valueOf(row)));
+            } else {
+                String seatName = seatAlphabetMap.get(i % 7) + row;
+                Seat.SeatStatus seatStatus = flight.getReservedSeats().contains(seatName) ? Seat.SeatStatus.UNAVAILABLE : Seat.SeatStatus.AVAILABLE;
+                seatList.add(new Seat(seatStatus, seatName));
             }
 
         }
 
-        SeatAdapter seatAdapter=new SeatAdapter(seatList, this, (selectedName, num) -> {
-            binding.numberSelectedTxt.setText(num+" Seat Selected");
+        SeatAdapter seatAdapter = new SeatAdapter(seatList, this, (selectedName, num) -> {
+            binding.numberSelectedTxt.setText(num + " Seat Selected");
             binding.nameSeatSelectedTxt.setText(selectedName);
-            DecimalFormat df=new DecimalFormat("#.##");
-            price=(Double.valueOf(df.format(num*flight.getPrice())));
-                this.num=num;
-            binding.priceTxtSeatList.setText("$"+price);
+            DecimalFormat df = new DecimalFormat("#.##");
+            price = (Double.valueOf(df.format(num * flight.getPrice())));
+            this.num = num;
+            binding.priceTxtSeatList.setText("$" + price);
         });
         binding.seatRecyclerView.setAdapter(seatAdapter);
         binding.seatRecyclerView.setNestedScrollingEnabled(false);
--- Ticket-Booking/app/src/main/java/com/shamnas/ticket_booking/activity/TicketDetailActivity.java
+++ Ticket-Booking/app/src/main/java/com/shamnas/ticket_booking/activity/TicketDetailActivity.java
@@ -1,6 +1,7 @@
 package com.shamnas.ticket_booking.activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
 import com.shamnas.ticket_booking.R;
 import com.shamnas.ticket_booking.databinding.ActivityTicketDetailBinding;
 import com.shamnas.ticket_booking.model.Flight;
@@ -21,6 +23,33 @@ public class TicketDetailActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityTicketDetailBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        
+        getIntentExtra();
+        setVariable();
 
+    }
+
+    private void setVariable() {
+        binding.backBtnTicket.setOnClickListener(v -> finish());
+        binding.fromTxt.setText(flight.getFromShort());
+        binding.fromSmallTxt.setText(flight.getFrom());
+        binding.toTxt.setText(flight.getTo());
+        binding.toShortTxt.setText(flight.getToShort());
+        binding.toSmallTxt.setText(flight.getTo());
+        binding.dateTxt.setText(flight.getDate());
+        binding.timeTxt.setText(flight.getTime());
+        binding.arrivalTxt.setText(flight.getArriveTime());
+        binding.classTxtTicket.setText(flight.getClassSeat());
+        binding.priceTxtTicket.setText("$"+flight.getPrice());
+        binding.airlinesTxt.setText(flight.getAirlineName());
+        binding.seatTxt.setText(flight.getPassenger());
+
+        Glide.with(TicketDetailActivity.this)
+                .load(flight.getAirlineLogo())
+                .into(binding.logo);
+    }
+
+    private void getIntentExtra() {
+        flight=(Flight) getIntent().getSerializableExtra("flight");
     }
 }
