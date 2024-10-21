011186a64eb403b3e85e9c7d61990230f11b2ade
Shamima-Nasrin10 <shamima48nasrin@gmail.com>
2024-10-21 13:58:53 +0600

    reg

--- Ticket-Booking/app/src/main/java/com/shamnas/ticket_booking/activity/BaseActivity.java
+++ Ticket-Booking/app/src/main/java/com/shamnas/ticket_booking/activity/BaseActivity.java
@@ -4,6 +4,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,16 +18,27 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         database = FirebaseDatabase.getInstance();
-        Window window = getWindow();
 
+        Window window = getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        // It's better to set decor fits system windows here, but defer hiding the status bar
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.setDecorFitsSystemWindows(false);
-            window.getInsetsController().hide(WindowInsets.Type.statusBars());
         }
+    }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
 
+        // Now it's safe to access the insets controller
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowInsetsController insetsController = getWindow().getInsetsController();
+            if (insetsController != null) {
+                insetsController.hide(WindowInsets.Type.statusBars());
+            }
+        }
     }
 }
